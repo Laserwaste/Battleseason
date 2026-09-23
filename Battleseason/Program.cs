2052f4ff8e9c91349fd0530d2c128e6349ab44e4
Laserwaste <permataalkautsar1@gmail.com>
2026-09-23 14:55:57 +0700

    Update Program.cs

--- Battleseason/Program.cs
+++ Battleseason/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿int battle = 0;
+int totalexp = 0;
+int exp = 0;
+Console.Write("Input your name: ");
+string name = Console.ReadLine();
+
+Console.Write("input battle number: ");
+int.TryParse(Console.ReadLine(), out battle);
+
+Console.Write("input exp number: ");
+int.TryParse(Console.ReadLine(), out exp);
+
+for (int i = 0; i < battle; i++)
+{
+    Console.WriteLine($"Battle {i + 1} | Exp: {exp} | Total Exp: {totalexp += exp}");
+}  
+
+Console.WriteLine("\n== Battle Complete ==");
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Total Battles: {battle}");
+Console.WriteLine($"Total Experience: {totalexp}");
